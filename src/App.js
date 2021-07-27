b143ceb0a22ca4cbf35e1707358fb36a546c6c1e
palakmalik5 <palakmalik5pm@gmail.com>
2021-07-26 23:37:28 -0700

    addede

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Weather from "./Weather.js";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Palak Malik and is{" "} 
-      <a 
-      href="https://github.com/palakmalik5/react-weather-app">
-        Open-source on Github
-         </a>
-      </footer>    
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+          <footer>
+            This project was coded by Palak Malik and is{" "} 
+            <a 
+              href="https://github.com/palakmalik5/react-weather-app">
+              Open-source on Github
+            </a>
+          </footer> 
+      </div>
     </div>
   );
   }
