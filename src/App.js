2023a4a257b62f6379be084051c8baf48c4940c9
palakmalik5 <palakmalik5pm@gmail.com>
2021-07-26 22:27:58 -0700

    App.js updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Palak Malik and is{" "} 
+      <a 
+      href="https://github.com/palakmalik5/react-weather-app">
+        Open-source on Github
+         </a>
+      </footer>    
+    </div>
   );
   }
