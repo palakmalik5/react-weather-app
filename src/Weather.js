8034b0cfc0c549b4eb6dc9d372b427406bda56de
palakmalik5 <palakmalik5pm@gmail.com>
2021-07-27 14:45:47 -0700

    css files

--- src/Weather.js
+++ src/Weather.js
@@ -9,23 +9,24 @@ export default function Weather() {
                 <div className="col-9">
                     <input type="search" placeholder="type a city..." className="form-control"/>
                         <div className="col-3">
-                            <input type="submit" value="Search" className="btn btn-primary"/>
+                            <input type="submit" value="Search" className=" search btn btn-primary w-100"/>
                         </div>
                 </div>
             </div>
         </form>
-        <h1>New York</h1>
+        <h3>New York</h3>
         <ul>
           <li>Wednesday 07:00</li>
           <li>Mostly Cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className=" col-6">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
               alt="Mostly Cloudy"
             />
-            6℃
+            <span className="temperature">6</span>
+            <span className="units">℃|℉</span>
           </div>
           <div className="col-6">
             <ul>
