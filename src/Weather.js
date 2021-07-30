8d19664c4aee67bb6bcfadab9269fcf2e0e69b93
palakmalik5 <palakmalik5pm@gmail.com>
2021-07-29 22:09:19 -0700

    search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,15 @@
 import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate.js";
+import WeatherInfo from "./WeatherInfo.js";
 
 export default function Weather(props) {
     const [weatherData, setweatherData] = useState({ready: false});
-    
+    const [city, setCity] = useState(props.defaultcity);
+   
     function handleResponse(response){
-        console.log(response.data);
        setweatherData({
-           ready: true,
+        ready: true,
          temperature: response.data.main.temp,
          humidity: response.data.main.humidity,
          description: response.data.weather[0].description,
@@ -20,55 +20,50 @@ export default function Weather(props) {
        });
     }
 
+    function handleSubmit(event){
+        event.preventDefault();
+        search();
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
+
+    }
+
+    function search() {
+      const apiKey = "4c4354034cf1207d77d642488c56f07c";
+      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+    }
+
+
 if(weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="type a city..."
                 className="form-control"
+                onChange={handleCityChange}
               />
+              </div>
               <div className="col-3">
                 <input
                   type="submit"
                   value="Search"
-                  className=" search btn btn-primary w-100"
+                  className=" btn btn-primary w-100"
                 />
-              </div>
             </div>
           </div>
         </form>
-        <h3>{weatherData.city}</h3>
-        <ul>
-          <li><FormattedDate date={weatherData.date} /></li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className=" col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="units">℃|℉</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
 } else {
-    const apiKey = "4c4354034cf1207d77d642488c56f07c";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
 }  
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import FormattedDate from "./FormattedDate.js";
+
+export default function WeatherInfo(props) {
+    return (
+    <div className="WeatherInfo">
+        <h3>{props.data.city}</h3>
+        <ul>
+          <li><FormattedDate date={props.data.date} /></li>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className=" col-6">
+            <img src={props.data.iconUrl} alt={props.data.description} />
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="units">℃|℉</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+        </div>
+        );
+}
