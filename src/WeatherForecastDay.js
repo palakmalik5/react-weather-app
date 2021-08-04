683a6ea6f45815703aa6b3ed0a0baed5d9efa94a
palakmalik5 <palakmalik5pm@gmail.com>
2021-08-03 21:03:39 -0700

    updated weatherforecastday

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -10,9 +10,19 @@ export default function WeatherForecastDay(props) {
       let temperature = Math.round(props.data.temp.min);
       return `${temperature}`;
     }
+
+    function day() {
+      let date = new Date(props.data.dt * 1000);
+      let day = date.getDay();
+
+      let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+      return days[day];
+    }
+
     return(
         <div>
-        <div className="Weatherforecast-day">{props.data[0].dt}</div>
+        <div className="Weatherforecast-day">{day()}</div>
             <WeatherIcon code={props.data[0].weather[0].icon} size={36} />
             <div className="Weatherforecast-temperatures">
               <span className="Weatherforecast-temperature-max">
